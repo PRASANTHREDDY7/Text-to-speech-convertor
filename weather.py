4f910dc3391e284fd7d2c0fb8c5eaebebcc51a8d
PRASANTHREDDY7 <prasanthmakireddy7@gmail.com>
2025-02-03 21:07:09 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,6 +1,6 @@
 import requests
 
-# Replace with your OpenWeatherMap API key
+#enter your api_key and this is my api_key
 API_KEY = "2aa93f093b41ba8fa397ef94b2b8c69d"
 BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
 
@@ -9,7 +9,7 @@ def get_weather(city_name):
     request_url = f"{BASE_URL}?q={city_name}&appid={API_KEY}&units=metric"
     response = requests.get(request_url)
 
-    # Check if the request was successful
+    # Check if the request was successful 200 means it is successful
     if response.status_code == 200:
         data = response.json()
         weather = data["weather"][0]["description"]
@@ -17,7 +17,7 @@ def get_weather(city_name):
         humidity = data["main"]["humidity"]
         wind_speed = data["wind"]["speed"]
 
-        # Display the weather information
+        # it will Display the weather information
         print(f"Weather in {city_name}:")
         print(f"- Condition: {weather}")
         print(f"- Temperature: {temperature}°C")
@@ -30,6 +30,6 @@ def main():
     print("Welcome to the Weather App!")
     city_name = input("Enter the city name: ")
     get_weather(city_name)
-
+#first the program starts sfrom here 
 if __name__ == "__main__":
     main()
